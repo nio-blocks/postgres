332e0c1809a571c02b6d77ce544ae6ec08670620
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-10 11:47:51 -0700

    split connect and disconnect functionality into commands

--- postgres_base_block.py
+++ postgres_base_block.py
@@ -5,15 +5,19 @@ from nio.properties import VersionProperty, StringProperty, IntProperty
 from psycopg2 import connect
 
 
-@command('Connection status', method='connection_status')
 @not_discoverable
+@command('Connection status', method='connection_status')
+@command('Reconnect', method='connect')
+@command('Disconnect', method='disconnect')
 class PostgresBase(Block):
     """A block for communicating with an postgres database.
 
     Properties:
-        host:
-        port:
-
+        host(str): hostname of the database to connect to
+        port(int): postgres port on the host to connect to
+        db_name(str): name of the database on the host
+        user_name(str): user name credential of the postgres server
+        password(str): password credential of the postgres server
     """
 
     version = VersionProperty('1.0.0')
@@ -32,16 +36,30 @@ class PostgresBase(Block):
 
     def configure(self, context):
         super().configure(context)
-        self._conn = connect(database=self.db_name(), user=self.user_name(),
-                             password=self.password(), host=self.host(),
-                             port=self.port())
-        self._cur = self._conn.cursor()
+        self.connect()
 
     def stop(self):
         self.logger.debug('closing postgres connection...')
-        self._cur.close()
-        self._conn.close()
+        self.disconnect()
         super().stop()
 
     def connection_status(self):
         return 'Connected' if self._conn.status else 'Not connected'
+
+    def connect(self):
+        # connect to the database and create the cursor object for executing
+        # commands
+        self.logger.debug('Connecting to postgres db...')
+        try:
+            self._conn = connect(database=self.db_name(), user=self.user_name(),
+                                 password=self.password(), host=self.host(),
+                                 port=self.port())
+            self._cur = self._conn.cursor()
+        except:
+            # fail to start the service if a connection can't be made
+            raise
+
+    def disconnect(self):
+        # disconnect from the database and close the cursor object
+        self._cur.close()
+        self._conn.close()
