c955d2ef174722114f44adf39c8dc588106fc1f8
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-11 12:47:25 -0700

    mogrify SQL query string instead of format

--- postgres_insert_block.py
+++ postgres_insert_block.py
@@ -1,9 +1,7 @@
 from nio.properties import BoolProperty
-from nio.util.discovery import discoverable
 from .postgres_base_block import PostgresBase
 
 
-@discoverable
 class PostgresInsert(PostgresBase):
     """A block for inserting incoming signals into a postgres database.
 
@@ -36,15 +34,22 @@ class PostgresInsert(PostgresBase):
 
     def _build_insert_query_string(self, data):
         """build an INSERT SQL query based on the incoming (dictionary)
-        data.
+        data. psycopg2 automatically escapes/sanitizes values passed into
+        execute and mogrify. First mogrifying the table name and data keys
+        assures escaping of those variables, and the data values will be
+        escaped when execute is called.
         """
-        query_base = 'INSERT INTO {} ({}) VALUES ({})'
-        # TODO: possible sql injection on db name and data keys?
-        query_final = query_base.format(self.table_name(),
-                                        ', '.join(list(data.keys())),
-                                        ', '.join(['%s' for i in range(len(data.keys()))]))
+        query_base = 'INSERT INTO %s (%s) VALUES (%s)'
+        query_final = self._cur.mogrify(query_base, [self.table_name(),
+                                                     ', '.join(list(data.keys())),
+                                                     ', '.join(['%s'] * len(data))
+                                                     ])
+
+        # get rid of quotations
+        query_final = query_final.replace(b"'", b'')
 
         self.logger.debug('built query string: {}'.format(query_final))
+
         return query_final
 
     def _rollback_transactions(self):
