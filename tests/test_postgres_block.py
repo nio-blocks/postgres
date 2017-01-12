735dda515daa325df1d4b7437c8919f4d480a202
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-12 12:31:59 -0700

    add unit tests

--- tests/test_postgres_block.py
+++ tests/test_postgres_block.py
@@ -1,19 +1,63 @@
-from nio.block.terminals import DEFAULT_TERMINAL
+from unittest.mock import patch, MagicMock
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..example_block import Example
+from ..postgres_insert_block import PostgresInsert
+import psycopg2
 
 
-class TestExample(NIOBlockTestCase):
+class TestInsertBlock(NIOBlockTestCase):
 
-    def test_process_signals(self):
+    @patch.object(PostgresInsert, "connect")
+    def test_query_building(self, patched_conn):
         """Signals pass through block unmodified."""
-        blk = Example()
-        self.configure_block(blk, {})
+        blk = PostgresInsert()
+        blk._conn = MagicMock()
+        blk._cur = MagicMock()
+        self.configure_block(blk, {'host': '127.0.0.1',
+                                   'port': 5432,
+                                   'db_name': 'dbname',
+                                   'table_name': 'tablename',
+                                   'log_level': 'DEBUG'
+                                   })
         blk.start()
-        blk.process_signals([Signal({"hello": "n.io"})])
+        blk.process_signals([Signal({"testattr": "testval"})])
         blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "n.io"})
+
+        self.assert_num_signals_notified(0)
+        self.assertTrue(patched_conn.called)
+
+    @patch.object(PostgresInsert, "connect")
+    def test_validation_table_name(self, patched_conn):
+        blk = PostgresInsert()
+        blk._conn = MagicMock()
+        blk._cur = MagicMock()
+
+        config = {'host': '127.0.0.1',
+                  'port': 5432,
+                  'db_name': 'dbname',
+                  'table_name': '',
+                  'log_level': 'DEBUG'
+                  }
+
+        # test table name validation
+        for name in ['1test2', ');-- testname', 'testname;', '"testname"',
+                     'test$name', 'test:name::']:
+            config['table_name'] = name
+            self.assertRaises(ValueError, self.configure_block, blk, config)
+
+    @patch.object(PostgresInsert, "connect")
+    def test_validation_data_keys(self, patched_conn):
+        blk = PostgresInsert()
+        blk._conn = MagicMock()
+        blk._cur = MagicMock()
+        self.configure_block(blk, {'host': '127.0.0.1',
+                                   'port': 5432,
+                                   'db_name': 'dbname',
+                                   'table_name': 'tablename',
+                                   'log_level': 'DEBUG'
+                                   })
+
+        # data key validation
+        for key in ['1test2', ');-- testname', 'testname;', '"testname"',
+                    'test$name', 'test:name::']:
+            self.assertRaises(ValueError, blk.execute_insert, {key: "testval"})
