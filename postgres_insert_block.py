06c480c3accfbab73a7e47a24d9816533931817b
Dylan Orzel <dylan.orzel@gmail.com>
2017-01-12 09:59:27 -0700

    change commit and rollback logic, validate data keys, get rid of mogrify

--- postgres_insert_block.py
+++ postgres_insert_block.py
@@ -2,32 +2,41 @@ from .postgres_base_block import PostgresBase
 
 
 class PostgresInsert(PostgresBase):
-    """A block for inserting incoming signals into a postgres database.
-
-    Properties:
-
-    """
+    """A block for inserting incoming signals into a postgres database."""
 
     def __init__(self):
         super().__init__()
 
     def process_signals(self, signals):
         """execute an insert command for all incoming signals"""
-        for signal in signals:
-            self.execute_insert(signal.to_dict())
-        # only commit transactions when all have been attempted
+
+        # if commits are needed, commit after succesfully executing all
+        # queries, else rollback all transactions if any of them do not work.
         if self.commit_all():
-            self._commit_transactions()
+            try:
+                for signal in signals:
+                    self.execute_insert(signal.to_dict())
+                self._commit_transactions()
+            except:
+                self.logger.exception("Could not execute insert query")
+                self._rollback_transactions()
+        else:
+            for signal in signals:
+                try:
+                    self.execute_insert(signal.to_dict())
+                except:
+                    self.logger.exception("Could not execute insert query")
 
     def execute_insert(self, data):
         """execute an insert query for the given data"""
         self.logger.debug('executing INSERT on data: {}'.format(data))
-        try:
-            self._cur.execute(self._build_insert_query_string(data),
-                              tuple(data.values()))
-        except:
-            self.logger.exception("Could not execute command".format())
-            self._rollback_transactions()
+
+        # first check to see if the keys are valid
+        for key in data.keys():
+            self._validate_string(key)
+
+        self._cur.execute(self._build_insert_query_string(data),
+                          tuple(data.values()))
 
     def _build_insert_query_string(self, data):
         """build an INSERT SQL query based on the incoming (dictionary)
@@ -36,14 +45,15 @@ class PostgresInsert(PostgresBase):
         assures escaping of those variables, and the data values will be
         escaped when execute is called.
         """
-        query_base = 'INSERT INTO %s (%s) VALUES (%s)'
-        query_final = self._cur.mogrify(query_base, [self.table_name(),
-                                                     ', '.join(list(data.keys())),
-                                                     ', '.join(['%s'] * len(data))
-                                                     ])
+
+        query_base = 'INSERT INTO {} ({}) VALUES ({})'
+        query_final = query_base.format(self.table_name(),
+                                        ', '.join(list(data.keys())),
+                                        ', '.join(['%s'] * len(data))
+                                        )
 
         # get rid of quotations
-        query_final = query_final.replace(b"'", b'')
+        query_final = query_final.replace("'", '')
 
         self.logger.debug('built query string: {}'.format(query_final))
 
